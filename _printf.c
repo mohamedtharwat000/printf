476588d2c3061e7b8caf42a3cc5b722015e83255
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-25 20:04:41 +0200

    test

--- _printf.c
+++ _printf.c
@@ -1,48 +1,46 @@
 #include "main.h"
 
 /**
- * _printf -printf function
+ * _printf - printf like function
  * @format: format.
  * Return: printed chars
 */
 
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
+	int i = 0, printed_char_specifier = 0, printed_chars_all = 0, buffer_i = 0;
+	int flags, width, precision, length;
+	char buffer[BUFF_SIZE];
+	va_list list;
 
-if (format == NULL)
-return (-1);
-
-va_start(list, format);
-for (i = 0; format && format[i] != '\0'; i++)
-{
-	if (format[i] != '%')
+	if (!format)
 	{
-		buffer[buff_ind++] = format[i];
-		if (buff_ind == BUFF_SIZE)
-			print_buffer(buffer, &buff_ind);
-		printed_chars++;
-	}
-	else
-	{
-		print_buffer(buffer, &buff_ind);
-		flags = get_flags(format, &i);
-		width = get_width(format, &i, list);
-		precision = get_precision(format, &i, list);
-		size = get_size(format, &i);
-		++i;
-		printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-		if (printed == -1)
 		return (-1);
-
-		printed_chars += printed;
 	}
-}
-print_buffer(buffer, &buff_ind);
-va_end(list);
-	return (printed_chars);
+
+	va_start(list, format);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1);
+			printed_chars_all++;
+		}
+		if (format[i] == '%')
+		{
+			i++;
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			length = get_size(format, &i);
+			printed_char_specifier = handle_print(format, &i, list, buffer,
+				flags, width, precision, length);
+			printed_chars_all += printed_char_specifier;
+		}
+		write(1, buffer, buffer_i);
+		buffer_i = 0;
+	}
+	va_end(list);
+
+	return (printed_chars_all);
 }
--- handle_print.c
+++ handle_print.c
@@ -1,48 +1,57 @@
 #include "main.h"
+
 /**
  * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
+ * @format: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
- * @ind: ind.
+ * @i: Index.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
+ * @flags: Calculates active flags.
+ * @width: Get width.
+ * @precision: Precision specification.
+ * @size: Size specifier.
+ *
+ * Return: Number of characters printed or -1 on failure.
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-int flags, int width, int precision, int size)
+
+int handle_print(const char *format, int *i, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
-int i, unknow_len = 0, printed_chars = -1;
-fmt_t fmt_types[] = {
-{'c', print_char}, {'s', print_string}, {'%', print_percent},
-{'i', print_int}, {'d', print_int}, {'b', print_binary},
-{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
-};
-for (i = 0; fmt_types[i].fmt != '\0'; i++)
-if (fmt[*ind] == fmt_types[i].fmt)
-return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-if (fmt_types[i].fmt == '\0')
-{
-if (fmt[*ind] == '\0')
-return (-1);
-unknow_len += write(1, "%%", 1);
-if (fmt[*ind - 1] == ' ')
-unknow_len += write(1, " ", 1);
-else if (width)
-{
---(*ind);
-while (fmt[*ind] != ' ' && fmt[*ind] != '%')
---(*ind);
-if (fmt[*ind] == ' ')
---(*ind);
-return (1);
-}
-unknow_len += write(1, &fmt[*ind], 1);
-return (unknow_len);
-}
-return (printed_chars);
+	int index, printed_chars = 0;
+	specifiers format_specifiers[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'i', print_int},
+		{'d', print_int},
+		{'b', print_binary},
+		{'u', print_unsigned},
+		{'o', print_octal},
+		{'x', print_hexadecimal},
+		{'X', print_hexa_upper},
+		{'p', print_pointer},
+		{'S', print_non_printable},
+		{'r', print_reverse},
+		{'R', print_rot13string},
+		{'\0', NULL}
+	};
+
+	for (index = 0; format_specifiers[index].format_char != '\0'; index++)
+	{
+		if (format[*i] == format_specifiers[index].format_char)
+		{
+			return (
+				format_specifiers[index].format_function(
+					list, buffer, flags, width, precision, size
+				)
+			);
+		}
+	}
+
+	while (format[*i] != '%')
+	{
+		(*i)--;
+	}
+	printed_chars += write(1, "%", 1);
+	return (printed_chars);
 }
